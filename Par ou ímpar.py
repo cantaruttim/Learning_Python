3109d47938f1b544da5a9aafdf81f4e179c850dd
Matheus Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2022-02-13 21:34:03 -0300

    Update Par ou ímpar.py

--- Par ou ímpar.py
+++ Par ou ímpar.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## Construa um programa que mostre se o número inserido no sistema é par ou ímpar; E se ele é divisível por qual número (2, 3, 4, 5, 6, 7, 8, 9, 10 ou 12)
-
-# In[22]:
+# ## Construa um programa que mostre se o número inserido no sistema é par ou ímpar;
 
 
 numero_digitado = int(input('Digite o número desejado '))
 print(f'O número digitado foi: {numero_digitado}')
 
 
-# In[23]:
-
-
-# Descreve se o número digitado é par ou ímpar
+# Descrevendo se o número digitado é par ou ímpar
 
 if numero_digitado % 2 == 0:
     print(f'O número {numero_digitado} é par')

160331e2db49cea1dab05f5dc660ac2e880e738a
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2022-12-14 20:38:09 -0300

    coef_

--- Linear Regression tutorial.py
+++ Linear Regression tutorial.py
@@ -1,4 +1,4 @@
-https://www.youtube.com/playlist?list=PLQVvvaa0QuDfKTOs3Keq_kaG2P55YRn5v
+# link : https://www.youtube.com/playlist?list=PLQVvvaa0QuDfKTOs3Keq_kaG2P55YRn5v
 
 -- REGRESSION
 #import numpy as np
@@ -67,14 +67,34 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
 
 style.use('fivethirtyeight')
 
-xs = np.array([1,2,3,4,5,6], dtype=np.float64)
-ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+# xs = np.array([1,2,3,4,5,6], dtype=np.float64)
+# ys = np.array([5,4,6,5,6,7], dtype=np.float64)
 
 # calculando m and b
 # b = mean(y) - m*mean(x)
+
+def create_dateset(hm, variance, step=2, correlation=False):
+    val = 1
+    ys = []
+    
+    for i in range(hm):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation = "pos":
+            val +=step
+        elif correlation and correlation == "neg":
+            val -= step 
+    xs = [i for i in range(len(ys))]
+    return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
+
+# we can chance those values and get different values of variance
+# it'll imply directly if the data is tidy or not to the regression line
+
+xs, ys = create_dataset(40, 40, 2, correlation="pos")
 
 def best_fit_slope_and_intercept(xs, ys):
     m = mean( ((mean(xs) * mean(ys)) - mean(xs*ys)) / ((mean(xs)*mean(xs)) - mean(xs**2)) )
@@ -91,10 +111,8 @@ regression_line = [(m*x) + b for x in xs]
 predict_x = 8
 predict_y = (m*predict_m)+b
 
-plt.scatter(xs, ys)
-plt.scatter(predict_x, predict_y, color='g')
-plt.plot(xs, regression_line)
-plt.show()
+r_squared = coeficient_of_determination(ys, regression_line)
+print(r_squared)
 
 ### Determinando acurácia 
 # R Squared Theory
@@ -103,7 +121,7 @@ plt.show()
 # r² = 1 - (SE\hat{y} / SE mean(y) )
 
 def squared_error(ys_original, ys_line):
-    return sum((ys_line - ys_original) ** 2) 
+    return sum((ys_line - ys_original) ** 2 ) 
  
 def coeficient_of_determination(ys_original,ys_line):
     y_mean_line = [ mean(ys_original) for y in ys_original ]
@@ -112,7 +130,10 @@ def coeficient_of_determination(ys_original,ys_line):
     return 1 - (squared_error_regression / squared_error_y_mean)
 
 
-
+plt.scatter(xs, ys)
+#plt.scatter(predict_x, predict_y, s=100, color='g')
+plt.plot(xs, regression_line)
+plt.show()
 
 
 

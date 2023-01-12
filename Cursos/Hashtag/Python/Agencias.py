91770687b5ec7e17783c4a9c8af62ab46ec8dd3c
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-12 11:56:18 -0300

    class Agencia

---
+++ Cursos/Hashtag/Python/Agencias.py
@@ -0,0 +1,43 @@
+
+
+
+# criando agencias do banco
+
+
+class Agencia:
+
+    def __init__(self, telefone, cnpj, numero):
+        self.telefone = telefone
+        self.cnpj = cnpj
+        self.numero = numero
+        self.clientes = []
+        self.caixa = 0
+        self.emprestimos = []
+
+    def verificar_caixa(self):
+        if self.caixa < 1000000:
+            print('Caixa abaixo do nível recomendado. Caixa atual {}'.format(self.caixa))
+        else:
+            print('O valor {} do caixa está ok'.format(self.caixa))
+
+    def emprestar_dinheiro(self, valor, cpf, juros):
+        if self.caixa > valor:
+            self.emprestimos.append((valor, cpf, juros))
+        else:
+            print('Empréstimo não é possível. Dinheiro não disponível em caixa')
+
+
+    def adicionar_cliente(self, nome, cpf, patrimonio):
+        self.clientes.append((nome, cpf, patrimonio))
+
+
+agencia1 = Agencia(22223333, 123213128367, 5467)
+agencia1.caixa = 8777666
+print(agencia1.verificar_caixa())
+
+
+agencia1.emprestar_dinheiro(5000, 12342333, 1.15)
+print(agencia1.emprestimos)
+
+agencia1.adicionar_cliente('Matheus', 123324312, 600000)
+print(agencia1.clientes)

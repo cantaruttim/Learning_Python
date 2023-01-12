ed02dcb9a8f8e1731d6cb834c94840782e795825
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-11 22:21:59 -0300

    Update Metodo e POO.py

--- Cursos/Hashtag/Python/Metodo e POO.py
+++ Cursos/Hashtag/Python/Metodo e POO.py
@@ -2,5 +2,13 @@
 Tudo no Python é um objeto. Portanto pensamos numa classe
 Portanto, apresentam Métodos específicos da classe
 
+classe Pessoa:
+
+métodos:      Atributos
+- Andar   :    10km
+- Pular   :    1,5m
+- Comer   :    1Kg de comida por dia
+- Aprender :   Python
 
 """
+

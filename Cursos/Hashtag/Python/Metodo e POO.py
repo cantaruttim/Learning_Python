39fdd62a21356b4cdb9ce9789272bc554d208bd1
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-11 23:07:46 -0300

    Update Metodo e POO.py

--- Cursos/Hashtag/Python/Metodo e POO.py
+++ Cursos/Hashtag/Python/Metodo e POO.py
@@ -15,9 +15,28 @@ E nossos próprios métodos
 Ex. class Stack
 
 """
-a = [1,2,3,4,5]
-nome = 'Matheus'
+# a=[1,2,3,4,5]
+#nome= 'Matheus'
 
-print(type(nome.capitalize())) # <class 'str'>
-print(type(a)) # <class 'list'>
+#print(type(nome.capitalize())) # <class 'str'>
+# print(type(a)) # <class 'list'>
+
+
+######################################################################################
+
+#Obs¹ Polimorfismo classe + subclasses
+
+class TV:
+
+    def __init__(self):
+        self.cor = 'preta'
+        self.canal = 'Netflix'
+        self.Ligado = False
+        self.tamanho = 55
+        self.volume = 10
+
+tv_sala = TV()
+tv_quarto = TV()
+
+print(tv_sala.cor())
 

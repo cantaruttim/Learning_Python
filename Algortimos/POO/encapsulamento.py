902c8fbcab4c995a30915b13af9456fe6318acba
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-11 21:47:05 -0300

    Update encapsulamento.py

--- Algortimos/POO/encapsulamento.py
+++ Algortimos/POO/encapsulamento.py
@@ -4,3 +4,21 @@
 public, protected, private
 """
 
+class BaseDeDados:
+    def __init__(self):
+        self.dados = {}
+
+    def inserir_cliente(self, id, nome):
+        if 'clientes' not in self.dados:
+            self.dados['clientes'] = {id: nome}
+        else:
+            self.dados['clientes'].update({id: nome})
+
+bd = BaseDeDados()
+bd.inserir_cliente(1, 'Otáveio')
+bd.inserir_cliente(2, 'Miranda')
+bd.inserir_cliente(3, 'Rosemary')
+
+print(bd.dados)
+
+

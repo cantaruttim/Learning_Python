60c17a463885bdd74caa35ddee5fcbc1145b3372
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-11 22:43:26 -0300

    Update Metodo e POO.py

--- Cursos/Hashtag/Python/Metodo e POO.py
+++ Cursos/Hashtag/Python/Metodo e POO.py
@@ -10,5 +10,14 @@ métodos:      Atributos
 - Comer   :    1Kg de comida por dia
 - Aprender :   Python
 
+Vamos criar nossas próprias Classes
+E nossos próprios métodos
+Ex. class Stack
+
 """
+a = [1,2,3,4,5]
+nome = 'Matheus'
+
+print(type(nome.capitalize())) # <class 'str'>
+print(type(a)) # <class 'list'>
 

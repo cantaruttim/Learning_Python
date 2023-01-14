055574c8bbb0c934026603aa9215c006148fe020
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-14 18:29:17 -0300

    POO

---
+++ Cursos/Hashtag/POO/estudos_POO.py
@@ -0,0 +1,24 @@
+class Meu_Objeto:
+
+    # inicialização da classe
+    def __init__(pessoa):
+        # criando os atributos do objeto Pessoa
+        pessoa.nome = 'Pedro'
+        pessoa.idade = 33
+        pessoa.peso = 65.8
+        pessoa.endereco = 'Rua dos Bananais, 2890'
+        print('Chamando o construtor')
+
+
+    def imprime(pessoa):
+        print('Olá meu nome é %s e eu tenho %d' %(pessoa.nome, pessoa.idade))
+
+
+# Chamando os construtores e atributos
+
+a = Meu_Objeto()
+a.imprime()
+print(a.peso)
+print(a.endereco)
+
+# help(Meu_Objeto)

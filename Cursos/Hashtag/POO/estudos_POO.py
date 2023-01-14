33126d151f22eaed2f67637d768d41b74b0877bf
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-14 19:42:21 -0300

    Update estudos_POO.py

--- Cursos/Hashtag/POO/estudos_POO.py
+++ Cursos/Hashtag/POO/estudos_POO.py
@@ -2,6 +2,12 @@
 """
 A função cria_conta, determina os parâmetros necessários para criar uma conta
 
+atributos:
+    numero (str) : mostra o número da conta
+    titular (str : mostra o nome do titular da conta
+    saldo (float) : mostra o saldo positivo ou negativo da conta do titular
+    limite (float) : determina o limite da conta
+
 """
 
 def cria_conta(numero, titular, saldo, limite):
@@ -23,9 +29,27 @@ def deposita(conta, valor):
     conta['saldo'] += valor
 
 
+"""
+A função saca, realiza a operação de saque;
+
+atributos:
+    conta (int) : determina o número da conta
+    valor (float) : determina o valor a ser depositado na conta
+
+"""
+
 def saca(conta, valor):
     conta['saldo'] -= valor
 
+
+"""
+A função extrato mostra o valor atual da conta;
+
+atributos:
+    conta (int) : determina o número da conta
+    
+
+"""
 
 def extrato(conta):
     print(f"número {conta['número']} \nsaldo : {conta['saldo']}")

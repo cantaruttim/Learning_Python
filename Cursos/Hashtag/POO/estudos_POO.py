6f73f6faa999be373818a4942be4d20609a9ff62
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-14 19:02:57 -0300

    Update estudos_POO.py

--- Cursos/Hashtag/POO/estudos_POO.py
+++ Cursos/Hashtag/POO/estudos_POO.py
@@ -1,33 +1,38 @@
-class Meu_Objeto:
 
-    # inicialização da classe
-    def __init__(pessoa):
-        # criando os atributos do objeto Pessoa
-        pessoa.nome = 'Pedro'
-        pessoa.idade = 33
-        pessoa.peso = 65.8
-        pessoa.endereco = 'Rua dos Bananais, 2890'
-        print('Chamando o construtor')
-
-
-    def imprime(pessoa):
-        print('Olá meu nome é %s e eu tenho %d' %(pessoa.nome, pessoa.idade))
-
-
-# Chamando os construtores e atributos
-
-a = Meu_Objeto()
+"""
+A função cria_conta, determina os parâmetros necessários para criar uma conta
 
 """
 
-# chamando um método de dentro da minha classe
-a.imprime()
-
-# printando algumas atributos da classe
-print(a.peso)
-print(a.endereco)
-
-# help(Meu_Objeto)
+def cria_conta(numero, titular, saldo, limite):
+    conta = {"número": numero, "titular": titular, "saldo": saldo, "limite":limite}
+    return conta
 
 
 """
+A função deposita, realiza a operação de depósito;
+
+atributos:
+    conta (int) : determina o número da conta
+    valor (float) : determina o valor a ser depositado na conta
+
+"""
+
+
+def deposita(conta, valor):
+    conta['saldo'] += valor
+
+
+def saca(conta, valor):
+    conta['saldo'] -= valor
+
+
+def extrato(conta):
+    print(f"número {conta['número']} \nsaldo : {conta['saldo']}")
+
+
+
+conta = cria_conta('123-4', 'Matheus', 120.0, 1000.0)
+deposita(conta, 500)
+extrato(conta)
+

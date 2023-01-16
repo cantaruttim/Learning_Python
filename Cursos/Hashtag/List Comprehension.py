6ff09db8efd1fe6181432717f2f25a029526175c
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-16 08:52:45 -0300

    Update List Comprehension.py

--- Cursos/Hashtag/List Comprehension.py
+++ Cursos/Hashtag/List Comprehension.py
@@ -1,7 +1,7 @@
 # LC é uma forma de percorrer uma lista de valores de uma forma mais direta
 # uma única linha de código
 
-
+"""
 
 preco = [450, 550.25, 156, 3200]
 produtos = ['vinho', 'cafeteira', 'microondas', 'iphone 3']
@@ -36,3 +36,29 @@ print(lista_aux)
 
 produtos = [produtos for preco, produtos in lista_aux]
 print(produtos)
+
+
+"""
+
+
+vendas_produtos = [
+                    ('ipod', 55810, 70550), 
+                    ('Notebook Asus,GFORCE i7', 110550, 115220), 
+                    ('IphoneX', 48655, 38560)
+
+                    ]
+
+listas21 = []
+listas22 = []
+for produto, vendas21, vendas22 in vendas_produtos:
+    listas21.append(vendas21)
+    listas22.append(vendas22)
+
+print(listas21, listas22)
+
+# Fazendo com LC
+listas21_2 = [vendas21 for produto, vendas21, vendas22 in vendas_produtos]
+print(listas21_2)
+
+print(max(listas21_2))
+print(min(listas21_2))

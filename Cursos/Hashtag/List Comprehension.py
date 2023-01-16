be99154913c7f3a60b3e286091c26702a016508c
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-16 08:45:01 -0300

    Update List Comprehension.py

--- Cursos/Hashtag/List Comprehension.py
+++ Cursos/Hashtag/List Comprehension.py
@@ -3,15 +3,36 @@
 
 
 
-preco = [450, 550.25, 156, 325]
+preco = [450, 550.25, 156, 3200]
 produtos = ['vinho', 'cafeteira', 'microondas', 'iphone 3']
 
 
 impostos = []
 for item in preco:
     impostos.append(item * 0.03)
-print(impostos)
+#print(impostos)
 
 
 impostos_lc = [ preco * 0.3 for preco in preco]
 print(impostos_lc)
+
+# podemos utilizar isso com uma função
+
+def imposto(preco, valor_imposto):
+    return preco * valor_imposto
+
+imposto_df_LC = [imposto(preco, 0.03) for preco in preco]
+
+#################################################################
+
+preco = [450, 550.25, 156, 3200]
+produtos = ['vinho', 'cafeteira', 'microondas', 'iphone 3']
+
+lista_aux = list(zip(preco, produtos))
+
+lista_aux.sort(reverse=True)
+print(lista_aux)
+
+
+produtos = [produtos for preco, produtos in lista_aux]
+print(produtos)

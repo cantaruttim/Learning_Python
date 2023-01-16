a838eb5bd810b1c17f499eb256b5cab8949a5aee
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-16 08:28:46 -0300

    Create List Comprehension.py

---
+++ Cursos/Hashtag/List Comprehension.py
@@ -0,0 +1,13 @@
+# LC é uma forma de percorrer uma lista de valores de uma forma mais direta
+# uma única linha de código
+
+
+
+preco = [450, 550.25, 156, 325]
+produtos = ['vinho', 'cafeteira', 'microondas', 'iphone 3']
+
+
+impostos = []
+for item in preco:
+    impostos.append(item * 0.03)
+print(impostos)

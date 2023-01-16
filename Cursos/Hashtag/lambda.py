c36a7f19092bb2ff080582d64207c092fac7cda4
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-16 10:27:01 -0300

    Update lambda.py

--- Cursos/Hashtag/lambda.py
+++ Cursos/Hashtag/lambda.py
@@ -16,10 +16,26 @@ produtos = [' ABCD12 ', 'abcd44', 'abC33  ', '  BeB1514', 'BEEB1244   ']
 
 for i, produto in enumerate(produtos):
     produtos[i] = padronizar_texto(produto)
-print(produtos) # retorna o valor do texto padronizado
+#print(produtos) # retorna o valor do texto padronizado
 
 # Usando a função map
 
 # cada elemento da minha lista de produtos irá passar pela função padronizar_texto
 produtos = list(map(padronizar_texto, produtos))
-print(produtos)
+#print(produtos)
+
+
+# Lambda Expression
+# é uma função anônima, usada quando precisamos retornar apenas um valor
+
+def minha_func(n):
+    return n ** 2
+
+print(minha_func(5))
+
+
+# fazendo por lambda expression
+
+m = lambda n : n ** 2
+print(m(5))
+

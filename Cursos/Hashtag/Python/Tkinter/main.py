8855afc0a992de462e724f55263aaf5ae0123eb2
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-28 14:59:19 -0300

    Update main.py

--- Cursos/Hashtag/Python/Tkinter/main.py
+++ Cursos/Hashtag/Python/Tkinter/main.py
@@ -7,7 +7,7 @@ janela.title("Cotação de Moedas")
 mensagem = tk.Label(text='Sistemas de Busca de Cotação de Moedas', foreground='white', bg='black') # cria o objeto
 mensagem.pack()  # coloca o objeto na janela
 
-mensagem2 = tk.Label(text='Selecione a moeda desejada', fg='white', bg='black', width=50, height=50)
+mensagem2 = tk.Label(text='Selecione a moeda desejada', fg='white', bg='black', width=50, height=10)
 mensagem2.pack()
 
 janela.mainloop()

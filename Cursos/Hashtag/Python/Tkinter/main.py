ec42084986ae5ee98a849629c08f5a98eb5b4538
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-28 15:13:31 -0300

    Update main.py

--- Cursos/Hashtag/Python/Tkinter/main.py
+++ Cursos/Hashtag/Python/Tkinter/main.py
@@ -3,11 +3,20 @@ import tkinter as tk
 janela = tk.Tk()
 
 janela.title("Cotação de Moedas")
+janela.rowconfigure(0, weight=1)
+janela.columnconfigure([0, 1], weight=1)
 
-mensagem = tk.Label(text='Sistemas de Busca de Cotação de Moedas', foreground='white', bg='black') # cria o objeto
-mensagem.pack()  # coloca o objeto na janela
+mensagem = tk.Label(text='Sistemas de Busca de Cotação de Moedas', foreground='white', bg='black', width=35, height=5) # cria o objeto
+mensagem.grid(row=0,column=0, columnspan=2, sticky="NSEW")  # coloca o objeto na janela
 
-mensagem2 = tk.Label(text='Selecione a moeda desejada', fg='white', bg='black', width=50, height=10)
-mensagem2.pack()
+mensagem2 = tk.Label(text='Selecione a moeda desejada')
+mensagem2.grid(row=1,column=0)
+
+# Entry: caixa de texto para o usuário
+
+moeda = tk.Entry()
+moeda.grid(row=1, column=1)
+
+
 
 janela.mainloop()
